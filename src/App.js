cc727773f0d98b3367cd18c172938c4cb6d074fb
SirJohn42Walker <111967604+SirJohn42Walker@users.noreply.github.com>
2022-11-10 15:26:34 +0100

    HTML markup with just a hint of css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>
+        <h1>React and More Tea</h1>
+      </header>
+      <article>
+        <img
+          src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+          alt="A confident human beeing"
+        />
+        <h2>Morty Smith</h2>
+      </article>
+      <navbar>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+        </ul>
+      </navbar>
+    </div>
+  );
 }
